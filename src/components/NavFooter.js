bd9ca648a2517b24e3d06fe42552baec4ec7cf45
itshdog <haydenwestphal@gmail.com>
2023-07-13 07:31:54 -0500

    update version

--- src/components/NavFooter.js
+++ src/components/NavFooter.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NavFooter() {
     return(
         <div className="navbar-footer">
-            v0.1.0 | Updated 7/11/2023
+            v0.1.1 | Updated 7/13/2023
             <div className="navbar-links">
                 <a href="https://github.com/itshdog">GitHub</a>
                 <a href="https://account.venmo.com/u/itshdog">Support Me</a>
